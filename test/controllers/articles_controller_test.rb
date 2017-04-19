c7e7399e72fe29af2f20b96d26ea3073f9d11c09
aoreki <ln330640653@126.com>
2017-04-19 21:34:02 +0800

    添加测试

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,8 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+  	get articles_url
+  	assert_response :success
+  end
 end
