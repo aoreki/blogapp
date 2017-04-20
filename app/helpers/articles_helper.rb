4a3e1880c9e0308cea5cc30d98b28edea577b1bc
aoreki <ln330640653@126.com>
2017-04-20 22:16:54 +0800

    测试helper模块，通过self.included中raise看到调用关系

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+	def test_article_helper
+		'test article helper'
+	end
+
+	# def self.included(receiver)
+	# 	p receiver
+	# 	raise __FILE__
+	# end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -7,5 +7,6 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
   	get articles_url
   	assert_response :success
+  	assert_select 'h1','Hello, Rails! test article helper'
   end
 end
