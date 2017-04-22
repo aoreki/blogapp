d4442380879e2a25db4a339710bbf9aedaaeb430
aoreki <ln330640653@126.com>
2017-04-22 22:04:57 +0800

    add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   	resources :comments
   end
   get '/signup',to:'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
