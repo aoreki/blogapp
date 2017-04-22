7d6d54e8b175a58b36d2a36dfab2b261270fed9b
aoreki <ln330640653@126.com>
2017-04-22 17:58:53 +0800

    add header and footer,create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'welcome/index'
   root 'welcome#index'
   resources :articles do
   	resources :comments
   end
+  get '/signup',to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
