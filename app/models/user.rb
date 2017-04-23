727b11fa1055c2585f9ad04cb4ae701245fb7c32
aoreki <ln330640653@126.com>
2017-04-23 15:13:32 +0800

    validate user model add user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: {maximum:255}, uniqueness: true
+	validates :email, presence: true, format: {with: /.+@.+/}, uniqueness: { case_sensitive: false }
+	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be unique" do 
+  	dup_user = @user.dup 
+  	@user.save
+  	assert_not dup_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "Foo@ExAMPle.CoM"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
