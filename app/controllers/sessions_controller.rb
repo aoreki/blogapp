d2c6298741a09d8e31129b8c3fffff46829742cc
aoreki <ln330640653@126.com>
2017-04-29 22:13:38 +0800

    创建登陆临时会话

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
+  		login(user)
   		redirect_to user
   	else
+  		flash.now[:danger] = '用户名不存在或密码不正确' 
   		render 'new'
   	end
+  end
+
+  def destroy
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+	def login(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(:id,session[:user_id])
+	end
+
+	def login?
+		!!@current_user
+	end
 end
