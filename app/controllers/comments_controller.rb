f745eef29e25375954435a3852750b5d1fc06a80
aoreki <ln330640653@126.com>
2017-06-05 20:51:46 +0800

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
 		end
 	end
 	private
-		def comment_params
-			params.require(:comment).permit(:content,:user_id,:micropost_id)
-		end
+	def comment_params
+		params.require(:comment).permit(:content,:user_id,:micropost_id)
+	end
 end
